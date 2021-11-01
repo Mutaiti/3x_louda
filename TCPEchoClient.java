55c5ddb6c25df8d4d61a515f83f19ec04c56c945
Mutaiti <89254519+Mutaiti@users.noreply.github.com>
2021-11-01 11:24:20 +0300

    Add files via upload

---
+++ TCPEchoClient.java
@@ -0,0 +1,57 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+public class TCPEchoClient {
+    private static InetAddress host;
+    private static final int PORT = 1234;
+
+    public static void main(String[] args){
+        try{
+            host = InetAddress.getLocalHost();
+        }
+        catch(UnknownHostException unEx) {
+            System.out.println("Host ID not found!");
+            System.exit(1);
+        }
+        accessServer();
+    }
+    private static void accessServer(){
+        Socket link = null;
+
+        try{
+            link = new Socket (host, PORT);
+
+            Scanner input = new Scanner(link.getInputStream());
+            PrintWriter output = new PrintWriter(link.getOutputStream(), true);
+
+            Scanner userEntry = new Scanner (System.in);
+            String message, response;
+            userEntry.close();
+
+            do{
+                System.out.println("Enter a message: ");
+                message = userEntry.nextLine();
+
+                output.println(message);
+                response = input.nextLine();
+
+                System.out.println("\n SERVER> " + response);
+            }while(!message.equals("***CLOSE***"));
+        }
+        catch(IOException ioEx){
+            ioEx.printStackTrace();
+        }
+        finally{
+            try{
+                System.out.println("\n Closing connection ...*");
+                link.close();
+            }
+            catch(IOException ioEx){
+                System.out.println("Unable to disconnect !");
+                System.exit(1);
+            }
+        }
+    }
+    
+}
